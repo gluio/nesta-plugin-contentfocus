84d40c7648acb1fe075970e8feeaf8571f67bf2b
Glenn Gillen <me@glenngillen.com>
2015-03-03 22:10:19 +1100

    Remove duplicate slashes.

--- lib/nesta-plugin-drop/client.rb
+++ lib/nesta-plugin-drop/client.rb
@@ -32,7 +32,7 @@ module Nesta
         end
 
         def self.files
-          files = RestClient.get "#{host}/files", {
+          files = RestClient.get "#{host}files", {
             accept: :json, x_nestadrop_version: Nesta::Plugin::Drop::VERSION }
           Yajl::Parser.parse files
         rescue RestClient::Unauthorized
@@ -44,7 +44,7 @@ module Nesta
           local_path = [Nesta::App.root, file].join("/")
           puts "Caching: #{local_path}"
           FileUtils.mkdir_p(File.dirname(local_path))
-          file_contents = open("#{host}/file?file=#{URI.encode(file)}",
+          file_contents = open("#{host}file?file=#{URI.encode(file)}",
             http_basic_authentication: userinfo).read
           File.open(local_path, 'w') do |fo|
             fo.write file_contents
