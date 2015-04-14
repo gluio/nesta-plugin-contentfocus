7954306ea435cb33afa9d80f5682164c5c597982
Glenn Gillen <me@glenngillen.com>
2015-04-14 23:05:26 +1000

    Update default markdown processor.

--- lib/nesta-plugin-drop/init.rb
+++ lib/nesta-plugin-drop/init.rb
@@ -1,4 +1,5 @@
 require 'redcarpet'
 require "nesta-plugin-drop/app"
-Tilt.prefer Tilt::RedcarpetTemplate
+Tilt.register Tilt::RedcarpetTemplate::Redcarpet2, 'markdown', 'mkd', 'md'
+Tilt.prefer Tilt::RedcarpetTemplate::Redcarpet2
 Nesta::Plugin::Drop::Client.bootstrap!
