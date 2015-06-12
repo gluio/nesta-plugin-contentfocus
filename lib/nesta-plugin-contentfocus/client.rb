bd789fce9546e553e66fff09fd9e30d32e33d2c7
Glenn Gillen <me@glenngillen.com>
2015-06-12 12:52:54 +1000

    Add debugging output for websocket connections.

--- lib/nesta-plugin-contentfocus/client.rb
+++ lib/nesta-plugin-contentfocus/client.rb
@@ -80,14 +80,18 @@ module Nesta
 
         def self.subscribe_to_updates
           if update_channel
+            Nesta::Plugin::ContentFocus.logger.debug "CONTENTFOCUS: Subscribing to update channel"
             update_channel.subscribe(URI.parse(update_channel_url).path.sub(%r{\A/},""))
             update_channel.bind('file-added') do |data|
+              Nesta::Plugin::ContentFocus.logger.debug "CONTENTFOCUS: Streaming file add received"
               cache_file(data["file"])
             end
             update_channel.bind('file-removed') do |data|
+              Nesta::Plugin::ContentFocus.logger.debug "CONTENTFOCUS: Streaming file remove received"
               remove_file(data["file"])
             end
             update_channel.bind('config-changed') do |data|
+              Nesta::Plugin::ContentFocus.logger.debug "CONTENTFOCUS: Streaming config update received"
               Thread.new do
                 sleep(rand(0.0 ... 3.0))
                 bounce_server!
