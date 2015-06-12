1446a190ba09c85bb8137bcd91002e5d2a3ac8de
Glenn Gillen <me@glenngillen.com>
2015-06-12 15:28:41 +1000

    Add endpoint to reload app config.

--- lib/nesta-plugin-contentfocus/routes.rb
+++ lib/nesta-plugin-contentfocus/routes.rb
@@ -34,6 +34,18 @@ module Nesta
               ""
             end
           end
+
+          app.put "/contentfocus" do
+            if !contentfocus_request?
+              status 404
+            else
+              Thread.new do
+                Nesta::Plugin::ContentFocus::Client.bounce_server!
+              end
+              status 200
+              ""
+            end
+          end
         end
       end
     end
