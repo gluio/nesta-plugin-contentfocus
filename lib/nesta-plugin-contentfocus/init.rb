cf740a8cd9af6c38d1657ac11021fcf80c678011
Glenn Gillen <me@glenngillen.com>
2015-07-23 10:57:40 +1000

    Which markdown renderer to use is a theme/template concern.

--- lib/nesta-plugin-contentfocus/init.rb
+++ lib/nesta-plugin-contentfocus/init.rb
@@ -1,5 +1,3 @@
 require 'redcarpet'
 require "nesta-plugin-contentfocus/app"
-Tilt.register Tilt::RedcarpetTemplate::Redcarpet2, 'markdown', 'mkd', 'md'
-Tilt.prefer Tilt::RedcarpetTemplate::Redcarpet2
 Nesta::Plugin::ContentFocus::Client.bootstrap!
